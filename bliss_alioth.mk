cb8ef947cd630f7f981168d5b2019e24b99c61be
cristianvaz <cristianvaz81993@gmail.com>
2022-05-09 13:01:02 +0200

    alioth: declare Face Unlock support

--- bliss_alioth.mk
+++ bliss_alioth.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
